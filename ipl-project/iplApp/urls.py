09af34e01f6e60be0055748c4e3a75a30eb938fd
D. Abhinav Reddy <abhinavreddydaida07@gmail.com>
2026-02-10 16:05:28 +0530

    created login and register

--- ipl-project/iplApp/urls.py
+++ ipl-project/iplApp/urls.py
@@ -3,4 +3,6 @@ from iplApp import views
 #create your urlpatterns here.
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
 ]
--- ipl-project/iplApp/views.py
+++ ipl-project/iplApp/views.py
@@ -3,3 +3,25 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to the IPL App Home Page!")
+
+def register(request):
+    if request.method=='POST':
+         username=request.POST.get('username')
+         email=request.POST.get('email')
+         password=request.POST.get('password')
+         confirm_password=request.POST.get('confirm_password')
+
+         print(username, email,password,confirm_password)
+         return HttpResponse("Registration successful")
+    else:    
+        return render(request,'register.html')
+    
+def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username') 
+        password=request.POST.get('password')
+        print(username,password)
+        return HttpResponse("Login successful")
+
+    else:
+        return render(request,'login.html')       
