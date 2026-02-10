96ff21b0a31097d7d52d294a8cf260f809373971
D. Abhinav Reddy <abhinavreddydaida07@gmail.com>
2026-02-11 00:01:18 +0530

    .

--- ipl-project/iplApp/migrations/0001_initial.py
+++ ipl-project/iplApp/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='franchise',
+            name='franchises',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
